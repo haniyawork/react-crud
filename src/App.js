22a075ddde76d27921b8e197da6a0f2f205a40ea
hani <haniyeh.work.a@gmail.com>
2023-10-31 18:13:35 +1030

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Create from './components/create';
+import Read from './components/read';
+import Update from './components/update';
+import {  BrowserRouter ,
+  Routes,
+  Route,
+  } from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+  
+          <Route exact path='/create' component={Create} >
+       </Route>
+          <Route exact path='/read' component={Read} >
+          </Route>
+
+        <Route path='/update' component={Update} >
+        </Route>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
